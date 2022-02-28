34dea4bbbda5cb4a67c7ce7a34acc234f3b65c23
tothadam98 <75333311+tothadam98@users.noreply.github.com>
2022-02-28 11:22:21 +0200

    final

---
+++ ProductApp/Helper/IntegerToDashDotConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Globalization;
+
+
+namespace ProductApp.Helper
+{
+    [ValueConversion(typeof(int), typeof(System.Windows.Media.DashStyles))]
+    class IntegerToUnderlineConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int.Parse(value.ToString()) > 1000) ?  DashStyles.Dot : DashStyles.DashDot;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
